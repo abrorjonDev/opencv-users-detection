5ee54fe4d28062284738ea962e7619e1ebf225df
abrorjonDev <axmadov.git@gmail.com>
2023-05-21 12:58:56 +0500

    init

---
+++ install.md
@@ -0,0 +1,35 @@
+# 1. Virtual muhit oching
+
+## in Linux OS
+`python3 -m venv venv`
+
+## in Windows OS
+`python -m venv venv`
+
+
+# 2. Virtual muhitni aktivlashtirish
+
+## in Linux OS
+`source venv/bin/activate`
+
+## in Windows OS
+### in CMD
+`venv\Scripts\activate.bat`
+### in Powershell
+`venv\Scripts\Activate.ps1`
+
+# 3. Kerakli paketlarni ustanovka qiling
+
+## in Linux OS
+`pip install --upgrade pip`
+`pip install -r requirements.txt`
+
+## in Windows OS
+`pip install -r requirements.txt`
+
+# 4. Dasturni ishga tushirish
+## in Linux OS
+`python main.py`
+
+## in Windows OS
+`python main.py` yoki `py main.py`
---
+++ main.py
@@ -0,0 +1,90 @@
+# import cv2
+
+# video_capture = cv2.VideoCapture(0)  # Use 0 for the default webcam
+
+# total_users = 0
+# previous_user_count = 0
+
+# while True:
+#     # Read the current frame from the video stream
+#     ret, frame = video_capture.read()
+
+#     # Perform object detection on the frame
+#     # Use the pre-trained model to detect faces or people in the frame
+#     # Update the user count based on the detected objects
+
+#     # Draw the user count on the frame
+#     cv2.putText(frame, "Users: " + str(total_users), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+#     # Display the frame with the user count
+#     cv2.imshow("Users Counter", frame)
+
+#     # Check for user interrupt to exit the loop
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
+
+# # Release the video capture object and close the windows
+# video_capture.release()
+# # cv2.destroyAllWindows()
+
+import cv2
+
+# https://github.com/opencv/opencv/blob/master/data/haarcascades
+
+# Load the pre-trained model for face detection
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+# Load the pre-trained model for person detection
+person_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_fullbody.xml')
+
+# Load the pre-trained model for person detection by upperbody
+person_cascade2 = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_upperbody.xml')
+
+video_capture = cv2.VideoCapture(0)  # Use 0 for the default webcam
+total_users = 0
+previous_user_count = 0
+
+while True:
+    # Read the current frame from the video stream
+    ret, frame = video_capture.read()
+
+    # Perform object detection on the frame
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+    # Detect people
+    people = person_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    # Detect people
+    people_upperbody = person_cascade2.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+    # Detect people
+    # people_by_upperbody = person_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+    # Update the user count based on the detected objects
+
+    if len(faces) > len(people):
+        total_users = len(faces) + len(people_upperbody)
+    current_user_count = len(people) # len(faces) # + len(people)
+    if current_user_count > previous_user_count:
+        total_users = current_user_count - previous_user_count + len(people_upperbody)
+    previous_user_count = current_user_count
+
+    # Add a frame around each detected user
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    # Draw the user count on the frame
+    cv2.putText(frame, "Users: " + str(total_users), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+    # Display the frame with the user count
+    cv2.imshow("Users Counter", frame)
+
+    # Check for user interrupt to exit the loop
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the video capture object and close the windows
+video_capture.release()
+cv2.destroyAllWindows()
